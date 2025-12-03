63d6f69a1c3c24c0a8902a5ccc6479314194804f
Cheeseworks <bluewitherer2422@gmail.com>
2025-12-03 12:50:25 -0500

    settings in editor

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 
 #include <Geode/Geode.hpp>
 
+#include <Geode/ui/GeodeUI.hpp>
+
 #include <Geode/modify/CCTextInputNode.hpp>
+#include <Geode/modify/EditorPauseLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -29,5 +32,32 @@ class $modify(ClipboardCCTextInputNode, CCTextInputNode) {
     void setTouchEnabled(bool value) {
         if (m_fields->menu) m_fields->menu->setVisible(value);
         CCTextInputNode::setTouchEnabled(value);
+    };
+};
+
+class $modify(ClipboardEditorPauseLayer, EditorPauseLayer) {
+    bool init(LevelEditorLayer * layer) {
+        if (!EditorPauseLayer::init(layer)) return false;
+
+        if (auto guidelinesMenu = static_cast<CCMenu*>(getChildByID("guidelines-menu"))) {
+            auto btnSprite = CircleButtonSprite::createWithSpriteFrameName("icon.png"_spr, 0.875f);
+            btnSprite->setScale(0.875f);
+
+            auto btn = CCMenuItemSpriteExtra::create(
+                btnSprite,
+                this,
+                menu_selector(ClipboardEditorPauseLayer::onClipboardBtn)
+            );
+            btn->setID("settings"_spr);
+
+            guidelinesMenu->addChild(btn);
+            guidelinesMenu->updateLayout(true);
+        };
+
+        return true;
+    };
+
+    void onClipboardBtn(CCObject*) {
+        openSettingsPopup(Mod::get());
     };
 };
