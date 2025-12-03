bf91865b2303c65dc3d622934bf17912f7450014
Cheeseworks <bluewitherer2422@gmail.com>
2025-12-03 12:22:45 -0500

    no editor

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,16 @@ using namespace geode::prelude;
 class $modify(ClipboardCCTextInputNode, CCTextInputNode) {
     struct Fields {
         Ref<ClipboardMenu> menu = nullptr;
+
+        bool noEditor = Mod::get()->getSettingValue<bool>("disable-editor");
     };
 
     bool init(float width, float height, char const* placeholder, char const* textFont, int fontSize, char const* labelFont) {
         if (!CCTextInputNode::init(width, height, placeholder, textFont, fontSize, labelFont)) return false;
 
         log::debug("hooked text node \"{}\"", getID());
+
+        if (LevelEditorLayer::get() && m_fields->noEditor) return true;
 
         m_fields->menu = ClipboardMenu::create(this);
         addChild(m_fields->menu);
